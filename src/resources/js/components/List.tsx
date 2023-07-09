2b4f94ee48acd1dcc397e3fc574ae712153d45d4
k-ichikawa <chottokeinkana@gmail.com>
2023-07-09 12:44:28 +0900

    fix list style

--- src/resources/js/components/List.tsx
+++ src/resources/js/components/List.tsx
@@ -7,25 +7,25 @@ export function List(props: { h1Text: React.ReactNode; }): React.ReactElement {
       <div className='photoItemList'>
         <a href='#' className='photoItem'>
           <div className='photoItemContent'>
-            <img src='/image/sample.jpeg'></img>
+            <img src='/image/sample.jpg'></img>
             <span>「One choice」MV衣装</span>
           </div>
         </a>
         <a href='#' className='photoItem'>
           <div className='photoItemContent'>
-            <img src='/image/sample.jpeg'></img>
+            <img src='/image/sample.jpg'></img>
             <span>「シーラカンス」MV衣装</span>
           </div>
         </a>
         <a href='#' className='photoItem'>
           <div className='photoItemContent'>
-            <img src='/image/sample.jpeg'></img>
+            <img src='/image/sample.jpg'></img>
             <span>「One choice」MV衣装2</span>
           </div>
         </a>
         <a href='#' className='photoItem'>
           <div className='photoItemContent'>
-            <img src='/image/sample.jpeg'></img>
+            <img src='/image/sample.jpg'></img>
             <span>「シーラカンス」MV衣装2</span>
           </div>
         </a>
